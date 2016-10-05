63458d7708207ce6de5e8c63bf0b34444ff4e28f
Sriram <sriram@Srirams-MacBook-Pro.local>
2016-10-04 20:07:36 -0400

    First Selenium Project

---
+++ Selenium.Framework/src/main/java/Hybrid/Selenium/Framework/Selenium_Test.java
@@ -0,0 +1,21 @@
+package Hybrid.Selenium.Framework;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Selenium_Test {
+
+	public static void main(String[] args) {
+		
+
+			
+			FirefoxDriver driver = new FirefoxDriver();
+			driver.get("http://www.google.com");
+			//WebElement test = driver.findElementById("gb_70");
+			//test.click();
+			         }
+			
+
+	
+
+}
